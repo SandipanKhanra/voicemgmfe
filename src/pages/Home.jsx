fe012a9793076ac52cca2044f3da8e5ff2d964a9
Sandipan Khanra <sandipankhanra@gmail.com>
2024-02-21 13:50:48 +0530

    Home page login and signup functionality added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,65 @@
 /* eslint-disable no-unused-vars */
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
 import React, { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+
 const pages = ["Dashboard"];
 const settings = ["Profile", "Account", "Logout"];
 const Home = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  function handleLogin() {
-    setIsLoggedIn(true);
-  }
+  const navigate = useNavigate();
   return (
-    <>
-      <Outlet />
-    </>
+    <Box width="100vw" height="90vh">
+      <Paper
+        sx={{
+          height: "50%",
+          width: "40%",
+          mx: "auto",
+          backgroundColor: "#edf5f3",
+          // justifyContent: "center",
+          // alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h2"
+          align="center"
+          sx={{ pt: 4, color: "#3f4040" }}
+        >
+          Welcome to VOICE Management
+        </Typography>
+        <Grid
+          container
+          display="flex"
+          justifyContent="center"
+          sx={{ p: 2, mt: 4 }}
+          spacing={2}
+        >
+          <Grid item>
+            <Button
+              variant="contained"
+              size="large"
+              sx={{ backgroundColor: "#581ad6" }}
+              startIcon={<LoginIcon />}
+              onClick={() => navigate("/auth")}
+            >
+              Login
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              startIcon={<PersonAddIcon />}
+              sx={{ borderColor: "#0fa632" }}
+              size="large"
+              onClick={() => navigate("/auth/signup")}
+            >
+              Signup
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Box>
   );
 };
 
